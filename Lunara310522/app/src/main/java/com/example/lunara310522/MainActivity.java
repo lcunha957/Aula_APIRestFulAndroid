e228b225cd26cb44977132d60f081a8754ae21de
Lunara Cunha <88806865+lcunha957@users.noreply.github.com>
2022-05-31 22:21:12 -0300

    corrigindo o código pro carregaImg
    corrigindo o código pro carregaImg

--- Lunara310522/app/src/main/java/com/example/lunara310522/MainActivity.java
+++ Lunara310522/app/src/main/java/com/example/lunara310522/MainActivity.java
@@ -27,11 +27,14 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btnTeste;
     private ImageView img;
+    // deixar o corpoReader como variavel global
+    private InputStreamReader corpoReader;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // sempre clicar em infer constraint no activity_main.xml para que a ImageView seja reconhecida.
         img =  (ImageView) findViewById(R.id.imgR);
         btnTeste = (Button) findViewById(R.id.button);
         btnTeste.setOnClickListener(new View.OnClickListener() {
@@ -43,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
                         // podemos colocar a nossa logica aqui, como por exemplo o acesso a rede;
                         // para consultar a http, usamos  a classe URL
                         try {
-                            URL githubEndpoint = new URL("https://dog.cep/api/breeds/image/random");
+                            URL githubEndpoint = new URL("https://dog.ceo/api/breeds/image/random");
                             //"https://api.github.com/");
                             // Para abrir a conexão
                             HttpsURLConnection conexao = (HttpsURLConnection) githubEndpoint.openConnection();
@@ -56,53 +59,95 @@ public class MainActivity extends AppCompatActivity {
                             conexao.setRequestProperty("Contact-Me", "cc21106@g.unicamp.br");
 
                             // Método getResponse: conecta e espera resposta
+                            // TO DO : TERMINAR O IF SE A CONEXAO FOR OK (IGUAL A 200)
                             if (conexao.getResponseCode() == 200) {
                                 // já obtenho os dados da resposta http
                                 InputStream corpo = conexao.getInputStream();
-                                InputStreamReader corpoReader = new InputStreamReader(corpo, "UTF-8");
-                                // aqui desmontamos o json e podemos usá-lo
-                                JsonReader jsonReader = new JsonReader(corpoReader);
-                                // aqui vou extrair as chaves do json recebido...
+                                 new carregaImg().execute();
+                               // corpoReader = new InputStreamReader(corpo, "UTF-8");
 
-                                jsonReader.beginObject(); // inicia o processamento do json
-                                while (jsonReader.hasNext()) { // faz um loop por todas as chaves
-                                    String key = jsonReader.nextName();
+                                //JsonReader jsonReader = new JsonReader(corpoReader);
+                                //jsonReader.beginObject(); // inicia o processamento do json
+                                //while (jsonReader.hasNext()) { // faz um loop por todas as chaves
+                                    //String key = jsonReader.nextName();
 
-                                    if (key.equals("message")) {
-                                        String local = jsonReader.nextString();
-                                        try {
-                                            InputStream in = new java.net.URL(local).openStream();
-                                            Bitmap fig = BitmapFactory.decodeStream(in);
-                                            img.setImageBitmap(fig);
-                                        } catch (Exception e) { // fim do try da linha 72
-                                            Log.e("Get image", e.getMessage());
+                                    //if (key.equals("message")) {
+                                        //String local = jsonReader.nextString();
+                                        //try {
+                                            //InputStream in = new java.net.URL(local).openStream();
+                                            //fig = BitmapFactory.decodeStream(in);
+
+//                                        } catch (Exception e) { // fim do try da linha 72
+  //                                          Log.e("Get image", e.getMessage());
                                         } // fim do catch da linha 76
                                         //if (key.equals("organization_url")) {
                                         //String value = jsonReader.nextString();
                                         //Log.e("CHAVE", String.format("\n chave %s valor %s \n", key, value));
-                                    } else { //fim do if da linha 70
-                                        jsonReader.skipValue();// pula para a próxima chave
-                                    } // fim do else da linha 82
+    //                                } else { //fim do if da linha 70
+      //                                  jsonReader.skipValue();// pula para a próxima chave
+                                    } finally // fim do else da linha 82
 
-                                    jsonReader.close();
-                                    conexao.disconnect();
+        //                            jsonReader.close();
+                                // aqui desmontamos o json e podemos usá-lo
+
                                 } //fim do while da linha 67
 // fim do if da linha 58 na linha 90
                             } else {
                                 Log.e("RESPONSE", "ERRO");
                                 // colocar uma label para indicar a falha
-                            }
+                            }  conexao.disconnect();
 // fim do try da linha 44 na linha 95
                         } catch (Exception e) {
                             Log.e("URL", e.getMessage());
                         } // fim do catch da linha 95
 
-                    }// fim do run da linha 41
+                    })// fim do run da linha 41
 
 
                 }); // fim do AsyncTask da linha 39
             } // fim do onClick da linha 38
-        }); // fim do onClickListener da linha 36
+        //}); // fim do onClickListener da linha 36
 
-    } // fim do onCreate da linha 31
+    //} // fim do onCreate da linha 31
+// TO DO: TERMINAR O MÉTODO DO CARREGAIMG
+    private class carregaImg extends AsyncTask<InputStreamReader, Void, Bitmap>{
+
+    public void carregaImg () {
+           }
+        @Override
+        protected Bitmap doInBackground(String...str){
+            Bitmap fig = null;
+            JsonReader jsonReader = new JsonReader(corpoReader);
+            // aqui vou extrair as chaves do json recebido...
+               try {
+                   jsonReader.beginObject(); // inicia o processamento do json
+                   while (jsonReader.hasNext()) { // faz um loop por todas as chaves
+                       String key = jsonReader.nextName();
+
+                       if (key.equals("message")) {
+                           String local = jsonReader.nextString();
+                           try {
+                               InputStream in = new java.net.URL(local).openStream();
+                               fig = BitmapFactory.decodeStream(in);
+                             break;
+                           } catch (Exception e) { // fim do try da linha 72
+                               Log.e("Get image", e.getMessage());
+                           } // fim do catch da linha 76
+                           //if (key.equals("organization_url")) {
+                           //String value = jsonReader.nextString();
+                           //Log.e("CHAVE", String.format("\n chave %s valor %s \n", key, value));
+                       } else { //fim do if da linha 70
+                           jsonReader.skipValue();// pula para a próxima chave
+                       } // fim do else da linha 82
+
+                       jsonReader.close();
+
+                   } catch(Exception e){}
+                   return fig
+               }
+        @Override
+        protected void onPostExecute(Bitmap result){
+                img.setImageBitmap(fig);
+        }
+        }
 } // fim fo AppCompactActivity da linha 25
